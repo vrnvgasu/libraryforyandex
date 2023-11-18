bb5bd9494c1f3e088429654a0fd6308236bfd5be
dmitrii sh <dmitrii@sh.com>
2023-11-18 17:23:15 +0300

    init

---
+++ src/main/java/ru/demo/libraryforyandex/LibraryforyandexApplication.java
@@ -0,0 +1,13 @@
+package ru.demo.libraryforyandex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryforyandexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryforyandexApplication.class, args);
+	}
+
+}
